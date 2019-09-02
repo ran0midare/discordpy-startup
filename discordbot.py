803f52fde320e34b4075b489e96a3205387dda14
ran0midare <50224788+ran0midare@users.noreply.github.com>
2019-09-03 01:33:32 +0900

    perminutes

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,17 @@
 #coding:UTF-8
 import discord
 from discord.ext import tasks
-from datetime import datetime 
 
-TOKEN = "NjE4MTEyNTI2NjI1OTMxMjcz.XW09JA.y2IJRVkpoQmnrQNIT0D7JCxuYec" #トークン
-CHANNEL_ID = 299037452373458944 #チャンネルID
+TOKEN = "NjE4MTEyNTI2NjI1OTMxMjcz.XW1DIA.pAul1jgdN_mPymMxobJdyGZN_n8" #トークン
+CHANNEL_ID = 299037452373458944  #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
 async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '01:30':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('おはよう')  
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')  
 
 #ループ処理実行
 loop.start()
