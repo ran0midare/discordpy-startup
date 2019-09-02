3fc1d23e0b883143f18c3f4c5e00379f7b3d41bb
ran0midare <50224788+ran0midare@users.noreply.github.com>
2019-09-03 01:44:58 +0900

    zihou kai

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,7 @@
-#coding:UTF-8
-import discord
-from discord.ext import tasks
-
-TOKEN = "NjE4MTEyNTI2NjI1OTMxMjcz.XW1DIA.pAul1jgdN_mPymMxobJdyGZN_n8" #トークン
-CHANNEL_ID = 299037452373458944  #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('時間だよ')  
-
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+@client.event
+async def on_ready():
+    while True:
+        if time.strftime('%H:%M:%S',time.localtime())=='01:50:00':
+            channel = client.get_channel('299037452373458944')
+            await client.send_message(channel, '勝手に喋るよ')
+            sleep(5)
