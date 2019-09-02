48cf1e12be0bc2dcfd3b86b8c1d64477ef18f4db
ran0midare <50224788+ran0midare@users.noreply.github.com>
2019-09-03 01:59:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 @client.event
 async def on_ready():
     while True:
-        if time.strftime('%H:%M:%S',time.localtime())=='01:50:00':
+        if time.strftime('%H:%M:%S',time.localtime())=='18:00:00':
             channel = client.get_channel('299037452373458944')
             await client.send_message(channel, '勝手に喋るよ')
             sleep(5)
