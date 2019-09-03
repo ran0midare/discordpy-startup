a649635d6c686ac2e1336fbfb25f2ed455eb9863
ran0midare <50224788+ran0midare@users.noreply.github.com>
2019-09-03 23:40:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,10 +3,13 @@ import discord
 from discord.ext import tasks
 from datetime import datetime 
 
-TOKEN = "NjE4MTEyNTI2NjI1OTMxMjcz.XW54xw.wyTy1d5ry-2xlYbRO0UV4pIzA74" #トークン
+TOKEN = os.environ['NjE4MTEyNTI2NjI1OTMxMjcz.XW54xw.wyTy1d5ry-2xlYbRO0UV4pIzA74'] #トークン
 CHANNEL_ID = 299037452373458944 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
@@ -20,4 +23,6 @@ async def loop():
 #ループ処理実行
 loop.start()
 # Botの起動とDiscordサーバーへの接続
+bot.run(TOKEN)
 client.run(TOKEN)
+
