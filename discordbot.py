db3e505117d82c32807988b264933f515526a7e6
ran0midare <50224788+ran0midare@users.noreply.github.com>
2019-09-03 23:59:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,19 @@
-#coding:UTF-8
-import discord
-from discord.ext import tasks
-from datetime import datetime 
+from discord.ext import commands
+import os
+import traceback
 
-TOKEN = os.environ['NjE4MTEyNTI2NjI1OTMxMjcz.XW54xw.wyTy1d5ry-2xlYbRO0UV4pIzA74'] #トークン
-CHANNEL_ID = 299037452373458944 #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '23:40':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('おはよう')  
 
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-bot.run(TOKEN)
-client.run(TOKEN)
-
+bot.run(token)
